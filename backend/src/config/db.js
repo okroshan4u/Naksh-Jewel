e0443dc8714856b9796e214f74756a5ad761cfd1
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-02-14 22:35:58 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
-  } catch (error) {
+  } catch (error) 
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
